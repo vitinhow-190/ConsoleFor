d4cb05bbf46223088706b9a72abaf9ffde41e8e4
Usuario <Usuario@CPD02-M07>
2024-04-16 11:45:17 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleFor/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace ConsoleFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { for (int i = 1; i <= 9; i++) 
+            {
+                Console.Write  (i);
+                for (int j = 1; j <= 9; j++) 
+                {
+                    if (i != j) 
+                    {
+                        Console.Write(j);
+                    }
+                }
+
+                Console.WriteLine("");
+            }
+ 
+            Console.ReadKey();
+        }
+
+
+      
+
+
+
+    }
+
+
+}
